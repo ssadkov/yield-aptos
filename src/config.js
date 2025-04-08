e828aeb93b561bd20093e34fb318856550074507
ssadkov <mail@sheremetev.info>
2025-04-09 00:47:28 +0500

    config git

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const SYSTEM_AGENT = {
+  mnemonic: process.env.AGENT_MNEMONIC,
+  address: process.env.AGENT_ADDRESS,
+  privateKeyHex: process.env.AGENT_PRIVATE_KEY,
+};
+
+export default SYSTEM_AGENT;
